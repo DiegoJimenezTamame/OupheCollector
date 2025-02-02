6ea1dc848d530b69fa4830aef098cd6e2dd7e1ec
diegojimeneztamame <djimeneztamame@gmail.com>
2025-02-02 16:38:58 +0100

    ISSUE PERSISTS. App is stable BUT: The SVG views are being loaded according to the Logs but not displayed on the CardDetailFragment.java
    TODO: Further improvement in the UI and finally achieving correct display of MTG symbols is required.
    
    Implemented some new activity_main_drawer.xml icons. Next step would be to implement them as full color .png images. For some reason they are not displayed properly when loaded directly as menu items in the activity_main_drawer.xml

--- app/src/main/java/local/ouphecollector/activities/MainActivity.java
+++ app/src/main/java/local/ouphecollector/activities/MainActivity.java
@@ -1,27 +1,33 @@
 package local.ouphecollector.activities;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.navigation.NavigationView;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import local.ouphecollector.R;
 import local.ouphecollector.databinding.ActivityMainBinding;
+import local.ouphecollector.databinding.AppBarMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private AppBarConfiguration appBarConfiguration;
     private NavController navController;
     private ActivityMainBinding binding;
+    private AppBarMainBinding appBarMainBinding;
     private static final String TAG = "MainActivity";
 
     @Override
@@ -30,10 +36,11 @@ public class MainActivity extends AppCompatActivity {
         Log.d(TAG, "onCreate called");
         // View Binding setup
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+        appBarMainBinding = AppBarMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
         // Set up the toolbar
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        Toolbar toolbar = appBarMainBinding.toolbar;
         setSupportActionBar(toolbar);
 
         // Set up Navigation Drawer
@@ -42,22 +49,46 @@ public class MainActivity extends AppCompatActivity {
 
         // Set up Navigation Controller
         NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment);
+                .findFragmentById(R.id.nav_host_fragment_content_main);
         if (navHostFragment != null) {
             navController = navHostFragment.getNavController();
         }
 
-        // Configure the navigation drawer
-        appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_collection, R.id.nav_decklist,
-                R.id.nav_profile, R.id.nav_wishlist, R.id.nav_search)
-                .setOpenableLayout(drawer)
-                .build();
-
         // Link the action bar and the navigation drawer
         if (navController != null) {
-            NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
             NavigationUI.setupWithNavController(navigationView, navController);
+        }
+        setMenuIcons(navigationView);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+    }
+
+    private Map<Integer, Integer> getMenuImages() {
+        Map<Integer, Integer> menuImages = new HashMap<>();
+        menuImages.put(R.id.nav_home, R.drawable.home_icon);
+        menuImages.put(R.id.nav_collection, R.drawable.collection_icon);
+        menuImages.put(R.id.nav_scanner, R.drawable.scanner_icon);
+        menuImages.put(R.id.nav_decklist, R.drawable.decklist_icon);
+        menuImages.put(R.id.nav_wishlist, R.drawable.shopping_cart_icon);
+        menuImages.put(R.id.nav_profile, R.drawable.profile_icon);
+        menuImages.put(R.id.nav_search, R.drawable.search_icon);
+        return menuImages;
+    }
+
+    private void setMenuIcons(NavigationView navigationView) {
+        Menu menu = navigationView.getMenu();
+        Map<Integer, Integer> menuImages = getMenuImages();
+
+        for (int i = 0; i < menu.size(); i++) {
+            MenuItem menuItem = menu.getItem(i);
+            Integer imageResId = menuImages.get(menuItem.getItemId());
+            if (imageResId != null) {
+                Drawable icon = ContextCompat.getDrawable(this, imageResId);
+                menuItem.setIcon(icon);
+            }
         }
     }
 
@@ -69,10 +100,6 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onSupportNavigateUp() {
-        if (navController != null) {
-            return NavigationUI.navigateUp(navController, appBarConfiguration)
-                    || super.onSupportNavigateUp();
-        }
-        return super.onSupportNavigateUp();
+        return navController.navigateUp() || super.onSupportNavigateUp();
     }
 }
